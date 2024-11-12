4f80bad7e9f8645ae8022343dda521bfdf8ee1a4
Yang Li <oliverlee2018@163.com>
2024-11-12 16:31:22 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # FedYOLO
 Running baseline for federated YOLOv8.
+
+## Create Env
+
+```bash
+conda create -n <ENV_NAME> python=3.10
+conda activate <ENV_NAME>
+python -m pip install flwr
+pip install ultralytics
+```
+
+## Running Scripts
+
+Always start the server first:
+
+```bash
+python3 server_assets/yolo_server.py --rounds 2 --min_num_clients 2 --sample_fraction 1.0
+```
+
+Then start the clients:
+
+```bash
+python3 client_0_assets/yolo_client_0.py --server_address=0.0.0.0:8080 --cid=0
+python3 client_1_assets/yolo_client_1.py --server_address=0.0.0.0:8080 --cid=1
+```
+
+## Note
+
+When running the scripts, if you encounter the following error, stating dataset images not found:
+
+```bash
+Note dataset download directory is '<SOME/DIRECTORY>'. You can update this in '<SOME/FILE.json>'
+```
+
+Please update the `datasets_dir` field in the `.json` file to the full path to the root directory of this repo. Such as:
+
+```bash
+"datasets_dir": "/home/<USERNAME>/Documents/GitHub/FedYOLO",
+```
+
---
+++ client_0_assets/yolo_client_0.py
@@ -0,0 +1,81 @@
+import argparse
+import warnings
+from collections import OrderedDict
+
+import torch
+
+import flwr as fl
+
+from ultralytics import YOLO
+
+
+parser = argparse.ArgumentParser(description='YOLO client')
+parser.add_argument(
+    "--server_address",
+    type=str,
+    default="0.0.0.0:8080",
+    help=f"gRPC server address (default '0.0.0.0:8080')",
+)
+parser.add_argument(
+    "--cid",
+    type=int,
+    required=True,
+    help="Client id. Should be an integer between 0 and NUM_CLIENTS",
+)
+
+warnings.filterwarnings("ignore", category=UserWarning)
+NUM_CLIENTS = 3
+
+# Need to avoid training or inferencing on the same model at the same time
+# https://docs.ultralytics.com/guides/yolo-thread-safe-inference/#why-should-each-thread-have-its-own-yolo-model-instance
+
+def train(net, epochs):
+    net.train(data="./client_0_assets/dummy_data_0/data.yaml", epochs=epochs, workers=0)
+
+def test(net):
+    """Validate the model on the specified dataset."""
+    results = net.val(data="./client_0_assets/dummy_data_1/data.yaml") # if don't work, use dummy_data_2
+    val_mAP50 = results.results_dict.get('metrics/mAP50(B)')
+    val_precision = results.results_dict.get('metrics/precision(B)')
+    loss = val_mAP50
+    accuracy = val_precision
+    net.train()
+    return loss, accuracy
+
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.net = YOLO("./client_0_assets/yolov8n_0.pt").to(self.device)
+
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in self.net.model.state_dict().items()]
+
+    def set_parameters(self, parameters, config):
+        params_dict = zip(self.net.model.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        self.net.model.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters, config)
+        train(self.net, config['epochs'])
+        return self.get_parameters(config), 10, {} # 10 is replacing the number of samples trained on this client
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters, config)
+        loss, accuracy = test(self.net)
+        return loss, len(parameters), {"accuracy": accuracy}
+
+
+def main():
+    args = parser.parse_args()
+    print(args)
+
+    assert args.cid < NUM_CLIENTS
+
+    fl.client.start_client(
+        server_address=args.server_address,
+        client=FlowerClient(),
+    )
+
+if __name__ == "__main__":
+    main()
