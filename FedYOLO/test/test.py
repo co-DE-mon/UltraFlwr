440fc11b34fdb619bb530ad01f9c968aa61c1ce7
Soumya Snigdha Kundu <soumya_snigdha.kundu@kcl.ac.uk>
2024-12-12 15:25:05 +0000

    Add faster testing to circumvent multiple yolo loads

--- FedYOLO/test/test.py
+++ FedYOLO/test/test.py
@@ -1,164 +1,76 @@
-import torch
-
 from ultralytics import YOLO
 from extract_final_save_from_client import extract_results_path
 
-import pandas as pd
+from FedYOLO.config import HOME, SERVER_CONFIG
 
-from FedYOLO.config import HOME, SPLITS_CONFIG, SERVER_CONFIG
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset_name', type=str, default='baseline')
+parser.add_argument('--strategy_name', type=str, default='FedAvg')
+parser.add_argument('--client_num', type=int, default=1)
+parser.add_argument('--scoring_style', type=str, default="client-client")
 
-DATASET_NAME = SPLITS_CONFIG['dataset_name']
-NUM_ROUNDS = SERVER_CONFIG['rounds']
-STRATEGY = SERVER_CONFIG['strategy']
+args = parser.parse_args()
 
-#! HUGE DIFFERENCES BETWEEN SYSTEMS REGARDING FILES PATHS AND LOGGING. NEED TO IN-DEPTH TEST THIS.
+dataset_name = args.dataset_name
+strategy_name = args.strategy_name
+client_num = args.client_num
+scoring_style = args.scoring_style
+num_rounds = SERVER_CONFIG['rounds']
 
-print('##################')
-print('# CLIENT RESULTS #')
-print('##################')
-print()
+def client_client_metrics(client_number, dataset_name, strategy_name):
 
-def get_client_metrics(client_number, dataset_name):
-    # Extract paths
-    client_model_weights_path = extract_results_path(f"{HOME}/logs/client_{client_number}_log_{dataset_name}_{STRATEGY}.txt")
-    weights = f"{HOME}/{client_model_weights_path}/weights/best.pt"
-    
-    # Load and validate local model
-    client_model = YOLO(weights)
-    client_metrics = client_model.val(data=f'{HOME}/datasets/{dataset_name}/partitions/client_{client_number}/data.yaml', verbose=False)
-    
-    # Create local model metrics table
-    client_table = pd.DataFrame({
-        'Class': list(client_metrics.names.values()),
-        'mAP@0.5:0.95': client_metrics.box.maps.tolist()
-    })
+    logs_path = f"{HOME}/logs/client_{client_number}_log_{dataset_name}_{strategy_name}.txt"
+    weights_path = extract_results_path(logs_path)
+    weights = f"{HOME}/{weights_path}/weights/best.pt"
+    model = YOLO(weights)
+    table = model.val(data=f'{HOME}/datasets/{dataset_name}/partitions/client_{client_number}/data.yaml', verbose=True)
+    return table
 
-    torch.cuda.empty_cache()
-    
-    # Extract global model weights
-    client_global_model_weights_path = extract_results_path(f"{HOME}/logs/client_{client_number}_log_{dataset_name}_{STRATEGY}.txt")
-    global_weights = f"{HOME}/{client_global_model_weights_path}/weights/best.pt"
-    
-    # Load and validate global model
-    client_global_model = YOLO(global_weights)
-    client_global_metrics = client_global_model.val(data=f'{HOME}/datasets/{dataset_name}/data.yaml', verbose=False)
-    
-    # Create global model metrics table
-    client_global_table = pd.DataFrame({
-        'Class': list(client_global_metrics.names.values()),
-        'mAP@0.5:0.95': client_global_metrics.box.maps.tolist()
-    })
+def client_server_metrics(client_number, dataset_name, strategy_name):
 
-    torch.cuda.empty_cache()
-    
-    # Combine local and global model metrics
-    combined_table = pd.merge(client_table, client_global_table, on='Class', how='inner')
-    combined_table.columns = ['Class', 'mAP@0.5:0.95_local', 'mAP@0.5:0.95_global']
+    logs_path = f"{HOME}/logs/client_{client_number}_log_{dataset_name}_{strategy_name}.txt"
+    weights_path = extract_results_path(logs_path)
+    weights = f"{HOME}/{weights_path}/weights/best.pt"
+    model = YOLO(weights)
+    table = model.val(data=f'{HOME}/datasets/{dataset_name}/data.yaml', verbose=True)
+    return table
 
-    del client_model
-    del client_global_model
-    
-    return combined_table
+def server_client_metrics(client_number, dataset_name, strategy_name, num_rounds):
 
-
-client_0_metrics_table = get_client_metrics(0, DATASET_NAME)
-client_1_metrics_table = get_client_metrics(1, DATASET_NAME)
-combined_table = pd.merge(client_0_metrics_table, client_1_metrics_table, on='Class', how='inner')
-combined_table.columns = ['Class', 'mAP@0.5:0.95_local_0', 'mAP@0.5:0.95_global_0', 'mAP@0.5:0.95_local_1', 'mAP@0.5:0.95_global_1']
-
-print()
-print()
-print('#####################################')
-print('# FINAL CONSOLIDATED CLIENT METRICS #')
-print('#####################################')
-print(combined_table.to_string(index=False))
-print()
-print()
-
-# clear variables and free memory
-del client_0_metrics_table
-del client_1_metrics_table
-del combined_table
-torch.cuda.empty_cache()
-
-print('##################')
-print('# SERVER RESULTS #')
-print('##################')
-print()
-
-torch.backends.cudnn.benchmark = True
-torch.set_num_threads(1) # https://github.com/ultralytics/yolov5/issues/2960
-
-def safe_get_metrics(metrics):
-    try:
-        classes = list(metrics.names.values())
-        maps = metrics.box.maps.tolist() if len(metrics.box.maps) > 0 else [0] * len(classes)
-        return classes, maps
-    except AttributeError:
-        # print(f"Warning: Metrics object doesn't have expected attributes. Returning empty lists.")
-        return [], []
-
-def create_safe_dataframe(metrics, client_name):
-    classes, maps = safe_get_metrics(metrics)
-    if len(classes) != len(maps):
-        print(f"Warning: Mismatch in length for {client_name}. Classes: {len(classes)}, mAP: {len(maps)}")
-        maps = maps + [0] * (len(classes) - len(maps))  # Pad with zeros if necessary
-    return pd.DataFrame({
-        'Class': classes,
-        f'mAP@0.5:0.95_{client_name}': maps
-    })
-
-def get_server_metrics(weights_path, dataset_name, training_strategy=None):
+    weights_path = f"{HOME}/weights/model_round_{num_rounds}_{dataset_name}_Strategy_{strategy_name}.pt"
     server_model = YOLO(weights_path)
-    normal_model = YOLO()
+    normal_model = YOLO(weights_path)
 
-    if training_strategy == 'FedHeadAvg': #! Need to make a config for this as well
+    if strategy_name == 'FedHeadAvg': #! Need to make a config for this as well
         detection_weights = {k: v for k, v in server_model.model.state_dict().items() if k.startswith('model.detect')}
         normal_model.model.load_state_dict({**normal_model.model.state_dict(), **detection_weights}, strict=False)   
         server_model = normal_model 
+    
+    table = server_model.val(data=f'{HOME}/datasets/{dataset_name}/partitions/client_{client_number}/data.yaml', verbose=True)
+    return table
 
-    server_model_metrics = server_model.val(data=dataset_name, verbose=True)
-    del server_model
-    torch.cuda.empty_cache()
-    return server_model_metrics
+def server_server_metrics(dataset_name, strategy_name, num_rounds):
 
-server_model_weights_path = f"{HOME}/weights/model_round_{NUM_ROUNDS}_{DATASET_NAME}_Strategy_{STRATEGY}.pt"
+    weights_path = f"{HOME}/weights/model_round_{num_rounds}_{dataset_name}_Strategy_{strategy_name}.pt"
+    server_model = YOLO(weights_path)
+    normal_model = YOLO(weights_path)
 
-server_model_client0_dataset = f'{HOME}/datasets/{DATASET_NAME}/partitions/client_0/data.yaml'
-server_model_client0_table = get_server_metrics(server_model_weights_path, server_model_client0_dataset)
-server_model_client0_table = create_safe_dataframe(server_model_client0_table, 'client_0')
+    if strategy_name == 'FedHeadAvg': #! Need to make a config for this as well
+        detection_weights = {k: v for k, v in server_model.model.state_dict().items() if k.startswith('model.detect')}
+        normal_model.model.load_state_dict({**normal_model.model.state_dict(), **detection_weights}, strict=False)   
+        server_model = normal_model 
+    
+    table = server_model.val(data=f'{HOME}/datasets/{dataset_name}/data.yaml', verbose=True)
+    return table
 
-server_model_client1_dataset = f'{HOME}/datasets/{DATASET_NAME}/partitions/client_1/data.yaml'
-server_model_client1_table = get_server_metrics(server_model_weights_path, server_model_client1_dataset)
-server_model_client1_table = create_safe_dataframe(server_model_client1_table, 'client_1')
-
-server_model_global_dataset = f'{HOME}/datasets/{DATASET_NAME}/data.yaml'
-server_model_global_table = get_server_metrics(server_model_weights_path, server_model_global_dataset)
-server_model_global_table = create_safe_dataframe(server_model_global_table, 'global')
-
-# Merge client based scores
-client_based_table = pd.merge(server_model_client0_table, server_model_client1_table, on='Class', how='inner')
-# Merge with global scores
-server_model_combined_table = pd.merge(client_based_table, server_model_global_table, on='Class', how='inner')
-
-# Fill NaN values with 0
-server_model_combined_table = server_model_combined_table.fillna(0)
-
-# Reorder columns if needed
-column_order = ['Class', 'mAP@0.5:0.95_client_0', 'mAP@0.5:0.95_client_1', 'mAP@0.5:0.95_global']
-server_model_combined_table = server_model_combined_table[column_order]
-
-# Convert mAP values to percentage and round to 2 decimal places
-for col in server_model_combined_table.columns[1:]:
-    server_model_combined_table[col] = (server_model_combined_table[col] * 100).round(2)
-
-print()
-print()
-print('#####################################')
-print('# FINAL CONSOLIDATED SERVER METRICS #')
-print('#####################################')
-print(server_model_combined_table.to_string(index=False))
-print()
-print()
-
-#! There is some multiprocessing issue. To trigger something has to be running in the background.
+if scoring_style == "client-client":
+    client_metrics_table = client_client_metrics(client_num, dataset_name, strategy_name)
+elif scoring_style == "client-server":
+    client_metrics_table = client_server_metrics(client_num, dataset_name, strategy_name)
+elif scoring_style == "server-client":
+    client_metrics_table = server_client_metrics(client_num, dataset_name, strategy_name, num_rounds)
+elif scoring_style == "server-server":
+    client_metrics_table = server_server_metrics(dataset_name, strategy_name, num_rounds)
+else:
+    raise ValueError(f"Invalid scoring_style: {scoring_style}")
---
+++ FedYOLO/test/test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Get the current directory
+CURRENT_DIR="$(pwd)"
+
+# Go back to steps from CURRENT_DIR to get the local home directory
+cd ../..
+
+# Echo the local home directory
+LOCAL_HOME="$(pwd)"
+
+CONFIG_FILE="$LOCAL_HOME/FedYOLO/config.py"
+
+# File path to the Python script
+PYTHON_SCRIPT="$LOCAL_HOME/FedYOLO/test/test.py"
+
+# Extract configurations from the Python config file using Python
+DATASET_NAME=$(python3 -c "import sys; sys.path.append('$LOCAL_HOME/FedYOLO'); import config; print(config.DATASET_NAME)")
+CONFIG_strategy=$(python3 -c "import sys; sys.path.append('$LOCAL_HOME/FedYOLO'); import config; print(config.SERVER_CONFIG['strategy'])")
+CLIENT_CONFIG_cid=$(python3 -c "import sys; sys.path.append('$LOCAL_HOME/FedYOLO'); import config; print(','.join(str(client['cid']) for client in config.CLIENT_CONFIG.values()))")
+
+# Convert the extracted variables to arrays in bash
+IFS=',' read -r -a datasets <<< "$DATASET_NAME"
+IFS=',' read -r -a strategies <<< "$CONFIG_strategy"
+IFS=',' read -r -a client_nums <<< "$CLIENT_CONFIG_cid"
+
+# Define scoring styles
+scoring_styles=("client-client" "client-server" "server-client" "server-server")
+
+# Loop through each combination of dataset, strategy, client number, and scoring style
+for dataset in "${datasets[@]}"; do
+  for strategy in "${strategies[@]}"; do
+    for client_num in "${client_nums[@]}"; do
+      for scoring_style in "${scoring_styles[@]}"; do
+        echo "Running with dataset=$dataset, strategy=$strategy, client_num=$client_num, scoring_style=$scoring_style"
+        python3 "$PYTHON_SCRIPT" --dataset_name "$dataset" --strategy_name "$strategy" --client_num "$client_num" --scoring_style "$scoring_style"
+        echo ""
+        echo ""
+      done
+    done
+  done
+done
