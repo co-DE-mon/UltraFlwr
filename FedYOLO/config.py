17135435dd80056d351f4d7a23a6410c7499a36b
co-DE-mon <rpranav.wrk@gmail.com>
2025-06-23 22:21:32 +0530

    Added BASE,local server address

--- FedYOLO/config.py
+++ FedYOLO/config.py
@@ -1,5 +1,7 @@
 # config.py
 import yaml
+import platform
+import os
 
 def get_nc_from_yaml(yaml_path):
     """Get number of classes from data.yaml file."""
@@ -18,12 +20,19 @@ def generate_client_config(num_clients, dataset_path):
     }
 
 # Base Configuration
-BASE = ""  # YOUR PATH CONTAINING UltraFlwr
-HOME = f"{BASE}/UltraFlwr"
+if 'microsoft' in platform.uname().release.lower():  # WSL/Bash
+    BASE = "/mnt/c/Users/Pranav Rustagi/Desktop/Project"
+else:
+    BASE = r"C:\Users\Pranav Rustagi\Desktop\Project"
+
+HOME = os.path.join(BASE, "UltraFlwr")
 DATASET_NAME = 'baseline'
-DATASET_PATH = f'{HOME}/datasets/{DATASET_NAME}'
-DATA_YAML = f"{DATASET_PATH}/data.yaml"
+DATASET_PATH = os.path.join(HOME, 'datasets', DATASET_NAME)
+DATA_YAML = os.path.join(DATASET_PATH, 'data.yaml')
+# print("DATA_YAML path:", DATA_YAML)
+# print("File exists:", os.path.exists(DATA_YAML))
 NC = get_nc_from_yaml(DATA_YAML)
+
 
 # Number of clients can be easily modified here
 NUM_CLIENTS = 2  # Change this to desired number of clients
@@ -43,7 +52,7 @@ SPLITS_CONFIG = {
 CLIENT_CONFIG = generate_client_config(NUM_CLIENTS, DATASET_PATH)
 
 SERVER_CONFIG = {
-    'server_address': "0.0.0.0:8080",
+    'server_address': "127.0.0.1:8080",
     'rounds': 2,
     'sample_fraction': 1.0,
     'min_num_clients': NUM_CLIENTS,
